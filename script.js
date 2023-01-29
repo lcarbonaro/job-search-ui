801d6e00aea5595f7f1ebdcecb11dbdd2c885266
Les Carbonaro <lcarbonaro@gmail.com>
2023-01-29 13:16:52 -0500

    first commit

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ let inp = document.querySelector('#inpCategory');
 
 btn.addEventListener('click', async () => {
     let requestData = { jobCategory: inp.value };
-    
+
     //let resp = await fetch('http://localhost:3000/searchbycategory',{    
-    let resp = await fetch('https://erin-eager-indri.cyclic.app/searchbycategory',{    
+    let resp = await fetch('https://erin-eager-indri.cyclic.app/searchbycategory', {
         method: 'POST', // *GET, POST, PUT, DELETE, etc. 
         headers: {
-            'Content-Type': 'application/json'            
-          },    
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(requestData) // body data type must match "Content-Type" header
     });
 
     let respJson = await resp.json();
 
     console.log(respJson);
-    
+
 });
